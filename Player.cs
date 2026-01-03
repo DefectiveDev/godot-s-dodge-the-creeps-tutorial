0f4250059d4c827df415fb98d461ebf1f7fba82a
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-01-03 18:13:35 -0500

    hide player on ready set animation sprite depending on movement direction

--- Player.cs
+++ Player.cs
@@ -11,6 +11,7 @@ public partial class Player : Area2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+	    Hide();
 	    ScreenSize = GetViewportRect().Size;
 	}
 
@@ -56,5 +57,18 @@ public partial class Player : Area2D
                     x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
                     y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
                 );
+
+
+        if (velocity.X != 0)
+        {
+            animatedSprite2D.Animation = "walk";
+            animatedSprite2D.FlipV = false;
+            animatedSprite2D.FlipH = velocity.X < 0;
+        }
+        else if (velocity.Y != 0)
+        {
+            animatedSprite2D.Animation = "up";
+            animatedSprite2D.FlipV = velocity.Y > 0;
+        }
 	}
 }
