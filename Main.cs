495cf8d640f0793ab92e2b0beb5c380a2055e366
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-01-04 14:45:11 -0500

    timers have gain functionality in main. Mobs spawn on path and given direction. NewGame will be launched from Main on ready until updated

--- Main.cs
+++ Main.cs
@@ -25,9 +25,42 @@ public partial class Main : Node
         GetNode<Timer>("StartTimer").Start();
     }
 
+    private void OnScoreTimerTimout()
+    {
+        _score++;
+    }
+
+    private void OnStartTimerTimeout()
+    {
+        GetNode<Timer>("MobTimer").Start();
+        GetNode<Timer>("ScoreTimer").Start();
+    }
+
+    private void OnMobTimerTimeout()
+    {
+        Mob mob = MobScene.Instantiate<Mob>();
+
+        var mobSpawnLocation = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
+        mobSpawnLocation.ProgressRatio = GD.Randf();
+
+        float direction = mobSpawnLocation.Rotation + Mathf.Pi / 2;
+
+        mob.Position = mobSpawnLocation.Position;
+
+        direction += (float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);
+
+        mob.Rotation = direction;
+
+        var velocity = new Vector2((float)GD.RandRange(150.0, 250.0), 0);
+        mob.LinearVelocity = velocity.Rotated(direction);
+
+        AddChild(mob);
+    }
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+	    NewGame();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
